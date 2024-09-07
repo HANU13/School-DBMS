4379e8e18f2606b987c1c50395c82fdb8dbaa446
HANU KOSHTI <95334298+HANU13@users.noreply.github.com>
2024-09-07 19:21:51 +0530

    Add files via upload

---
+++ src/schooljmcp2/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package schooljmcp2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new intro().setVisible(true); // TODO code application logic here
+    }
+
+}
